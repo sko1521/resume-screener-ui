9a220f11a1c99cd0354ebfdce1c3769adc696ecd
sko1521 <sudhir.kulat@springernature.com>
2020-01-24 14:04:13 +0530

    added modal

--- src/components/user/JobList.js
+++ src/components/user/JobList.js
@@ -9,6 +9,12 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const StyledTableCell = withStyles(theme => ({
     head: {
@@ -27,8 +33,20 @@ const useStyles = makeStyles({
 });
 
 const JobList = ({jobs})=>{
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = (jd) => {
+        setOpen(true);
+        console.log(jd)
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     const classes = useStyles();
     return (
+    <div>
         <TableContainer component={Paper}>
           <Table className={classes.table} aria-label="simple table">
             <TableHead>
@@ -47,12 +65,37 @@ const JobList = ({jobs})=>{
                   </TableCell>
                   <TableCell align="right">{jobs.jobTitle}</TableCell>
                   <TableCell align="right">{jobs.experience}</TableCell>
-                  <TableCell align="right"><Button variant="contained" color="primary">View</Button></TableCell>
+                  <TableCell align="right"><Button variant="contained" color="primary" onClick={()=>handleClickOpen(jobs.jd)}>View</Button></TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Job Description</DialogTitle>
+        <DialogContent>
+          <DialogContentText value="ddcc">
+            JD: Should have strong knowledge in HTML, CSS, JS
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Apply
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
       );
 }
 
